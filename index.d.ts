6b97520aa430ac8492d1af586109dbd0aa163958
Evan Jacobs <570070+probablyup@users.noreply.github.com>
2021-06-04 11:59:58 -0400

    add bundled typings (#1)

---
+++ index.d.ts
@@ -0,0 +1,26 @@
+import { TextFieldProps } from "@material-ui/core";
+import * as React from "react";
+
+export type MuiPhoneNumberProps = TextFieldProps & {
+  autoFormat?: boolean;
+  classes?: any;
+  countryCodeEditable?: boolean;
+  defaultCountry?: string;
+  disableAreaCodes?: boolean;
+  disableCountryCode?: boolean;
+  disableDropdown?: boolean;
+  dropdownClass?: string;
+  enableLongNumbers?: boolean;
+  excludeCountries?: string[];
+  inputClass?: string;
+  onChange: (
+    e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement> | string
+  ) => void;
+  onlyCountries?: string[];
+  preferredCountries?: string[];
+  regions?: [string] | string;
+};
+
+declare const MuiPhoneNumber: React.FC<MuiPhoneNumberProps>;
+
+export default MuiPhoneNumber;
